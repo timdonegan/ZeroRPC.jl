434219a333995ddc99a7b0295a3398394313e15c
Tim Donegan <tim@secondspectrum.com>
2014-06-10 20:14:28 -0700

    example of creating a server

---
+++ examples/TestServer.jl
@@ -0,0 +1,20 @@
+import Zerorpc
+
+module TestServer
+
+function hello(name)
+  ret = @sprintf "Hello, %s" name
+  return ret
+end
+
+function add(a, b)
+  return a+b
+end
+
+export hello, add
+
+end # module
+
+server = Zerorpc.Server(TestServer)
+Zerorpc.bind(server, "tcp://127.0.0.1:4242")
+Zerorpc.run(server)
